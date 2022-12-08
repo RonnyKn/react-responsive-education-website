1414f4d19405510edcfad6797076ae0aa1b3b7c3
RonnyKn <ronnykurniawn@gmail.com>
2022-12-08 14:46:36 +0700

    adding data FAQS

--- src/assets/data/Data.js
+++ src/assets/data/Data.js
@@ -67,3 +67,35 @@ export const popularCourse = [
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate quos architecto facilis.",
   },
 ]
+export const qa = [
+  {
+    question: "Who should take an Online Course?",
+    answer:
+      "Students join our online courses for a variety of reasons and come from diverse backgrounds, professions, and age groups. With online courses",
+  },
+  {
+    question: "What should I do if I need to start the course late?",
+    answer:
+      "It is recommended that you begin your course promptly. If you do need to start the course a few days late, contact your instructor as soon as possible after the course has opened to discuss your options. Registration closes on the Friday after the course begins.",
+  },
+  {
+    question: "What should I do if I need to start the course late?",
+    answer:
+      "It is recommended that you begin your course promptly. If you do need to start the course a few days late, contact your instructor as soon as possible after the course has opened to discuss your options. Registration closes on the Friday after the course begins.",
+  },
+  {
+    question: "How do I select a course?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium error facilis unde nesciunt sunt totam beatae voluptatem quia laborum perspiciatis, quisquam quos nam labore excepturi",
+  },
+  {
+    question: "I am a high school student, can I join?",
+    answer:
+      "Courses are open to all students 18 years or older. If you are under 18 years old, please contact us (online@sothebysinstitute.com) to discuss further.i",
+  },
+  {
+    question: "Can I take more than one course at a time?",
+    answer:
+      "If you are new to online learning, we recommend that you begin with one course. As our courses are offered frequently throughout the year, you can always come back and continue your studies with one or more topics",
+  },
+]
